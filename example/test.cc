8416688fc3e74e7ed389f47cc39fbc71044c7d23
Pranav Goswami <pranav.goswami92@gmail.com>
2015-06-28 21:47:45 +0530

    modifiing code for XIA

--- example/test.cc
+++ example/test.cc
@@ -9,25 +9,38 @@
 using cuckoofilter::CuckooFilter;
 
 int main(int argc, char** argv) {
-    size_t total_items  = 1000000;
+    size_t total_items  = 10000000;
 
     // Create a cuckoo filter where each item is of type size_t and
     // use 12 bits for each item:
     //    CuckooFilter<size_t, 12> filter(total_items);
-    // To enable semi-sorting, define the storage of cuckoo filter to be
+    // To enable semi-sorting, define the storage ofp cuckoo filter to be
     // PackedTable, accepting keys of size_t type and making 13 bits
     // for each key:
     //   CuckooFilter<size_t, 13, cuckoofilter::PackedTable> filter(total_items);
-    CuckooFilter<size_t, 12> filter(total_items);
+    CuckooFilter<size_t, 32> filter(total_items);
 
-    // Insert items to this cuckoo filter
+
+    //Insert XIA_type vaules to this CuckooFilter
+    int fclose(FILE *a_file);
+    FILE *fp;
+    fp=fopen("/Users/Pranav/Desktop/input_xia.txt", "r");
     size_t num_inserted = 0;
+    while(1) {
+        char i = fgetc(fp);
+        num_inserted++ ;
+        if (filter.Add(i) != cuckoofilter::Ok) {
+            break;
+        }
+    }
+    // Insert items to this cuckoo filter
+  /*  size_t num_inserted = 0;
     for (size_t i = 0; i < total_items; i++, num_inserted++) {
         if (filter.Add(i) != cuckoofilter::Ok) {
             break;
         }
     }
-
+*/
     // Check if previously inserted items are in the filter, expected
     // true for all items
     for (size_t i = 0; i < num_inserted; i++) {
